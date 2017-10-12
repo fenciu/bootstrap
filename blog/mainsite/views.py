c5d5eaf45d4dd4845189e20cb1db873b38e6ca08
YourName <886369@163.com>
2017-10-12 12:43:29 +0800

    2017.10.12 12:43

--- blog/mainsite/views.py
+++ blog/mainsite/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render,render_to_response
 from .models import article,article_pic,article_tag
+
 # Create your views here.
 #http://www.cnblogs.com/yangmv/p/5327477.html
 def index(request):
-    articles=article.objects.all().values('title','author','date','content')[:1]
+    articles=article.objects.all().values('title','author','date','content')
     #return render_to_response('index.html',articles)
+    print (articles[0]['title'])
     return render(request,'index.html',{'articles':articles})
