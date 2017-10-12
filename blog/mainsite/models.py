8de0e3315d36d4e3afdf2e7e82ad55fc44d8a92a
Lades <886369@163.com>
2017-10-12 09:36:41 +0800

    11:25

--- blog/mainsite/models.py
+++ blog/mainsite/models.py
@@ -4,16 +4,23 @@ from django.db import models
 class article(models.Model):
     title=models.CharField(max_length=50)
     author=models.CharField(max_length=30)
-    date=models.DateField()
-    content=models.CharField
-    
+    date=models.DateTimeField()
+    content=models.TextField(default=None)
+    def __str__(self):
+        return self.title
+    class Meta:
+        ordering=['date']
 
 
 class article_pic(models.Model):
     article_id=models.IntegerField()
     url=models.FileField(upload_to='./upload/')
+    def __str__(self):
+        return self.url
 
 class article_tag(models.Model):
     article_id=models.IntegerField()
     tag_name=models.CharField(max_length=30)
+    def __str__(self):
+        return self.tag_name
 
--- blog/mainsite/views.py
+++ blog/mainsite/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,render_to_response
+from .models import article,article_pic,article_tag
 # Create your views here.
+#http://www.cnblogs.com/yangmv/p/5327477.html
 def index(request):
-    return render(request,'index.html')
+    articles=article.objects.all().values('title','author','date','content')[:1]
+    #return render_to_response('index.html',articles)
+    return render(request,'index.html',{'articles':articles})
